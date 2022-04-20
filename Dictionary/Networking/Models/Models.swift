745e674287e1c9dc97523effd362c11f5ef7d5f8
Dmitry Likhaletov <likhaletov.d@gmail.com>
2022-04-20 18:07:05 +0300

    add word model

---
+++ Dictionary/Networking/Models/Models.swift
@@ -0,0 +1,13 @@
+//
+//  Models.swift
+//  Dictionary
+//
+//  Created by Лихалетов Дмитрий Михайлович on 20.04.2022.
+//
+
+import Foundation
+
+struct Word: Codable {
+    let id: Int
+    let text: String
+}
