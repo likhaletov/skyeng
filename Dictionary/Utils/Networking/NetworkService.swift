88b98fd4cb14f3395533e4bc533616f94e0fd8cd
Dmitry Likhaletov <likhaletov.d@gmail.com>
2022-04-21 10:49:19 +0300

    little fix in the network service

--- Dictionary/Utils/Networking/NetworkService.swift
+++ Dictionary/Utils/Networking/NetworkService.swift
@@ -7,17 +7,29 @@
 
 import Foundation
 
+enum Errors: Error {
+    case invalidUrl
+    case invalidData
+}
+
 protocol NetworkServiceProtocol {
     func request(_ endpoint: Endpoint, completion: @escaping (Result<Data, Error>) -> Void)
 }
 
 struct NetworkService: NetworkServiceProtocol {
     func request(_ endpoint: Endpoint, completion: @escaping (Result<Data, Error>) -> Void) {
-        guard let url = endpoint.url else { completion(.failure(NSError())); return }
-        print(url)
+        guard let url = endpoint.url else { completion(.failure(Errors.invalidUrl)); return }
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else { return }
+            if let error = error { completion(.failure(error)); return }
+            guard let response = response as? HTTPURLResponse, response.isSuccess else { return }
+            guard let data = data else { completion(.failure(Errors.invalidData)); return }
             completion(.success(data))
         }.resume()
+    }
+}
+
+extension HTTPURLResponse {
+    var isSuccess: Bool {
+        return (200...299).contains(self.statusCode)
     }
 }
